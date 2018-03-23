4d74a841206903c2ba87f8e09efcf7a354e6e642
Stanislas Polu <polu.stanislas@gmail.com>
2018-03-23 21:37:30 +0100

    fix update script

--- scripts/update_experiment.sh
+++ scripts/update_experiment.sh
@@ -15,13 +15,13 @@ while [ true ]; do
 
   RUNTIME=$((`date +%s`-$START))
   echo "[Update] experiment=$EXPERIMENT tempdir=$TEMP runtime=$RUNTIME"
-  grep step $temp/out.log | cut -d' ' -f6 | gnuplot -p -e 'set terminal png; set output "'$temp'/fps.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f9 | gnuplot -p -e 'set terminal png; set output "'$temp'/mean.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f10 | gnuplot -p -e 'set terminal png; set output "'$temp'/median.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f13 | gnuplot -p -e 'set terminal png; set output "'$temp'/min.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f14 | gnuplot -p -e 'set terminal png; set output "'$temp'/max.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f16 | gnuplot -p -e 'set terminal png; set output "'$temp'/loss_entropy.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f18 | gnuplot -p -e 'set terminal png; set output "'$temp'/loss_value.png"; plot "/dev/stdin";'
-  grep step $temp/out.log | cut -d' ' -f20 | gnuplot -p -e 'set terminal png; set output "'$temp'/loss_action.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f6 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/fps.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f9 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/mean.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f10 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/median.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f13 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/min.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f14 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/max.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f16 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/loss_entropy.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f18 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/loss_value.png"; plot "/dev/stdin";'
+  grep step $TEMP/out.log | cut -d' ' -f20 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/loss_action.png"; plot "/dev/stdin";'
   grep STAT $TEMP/out.log | cut -d' ' -f3 | gnuplot -p -e 'set terminal png; set output "'$TEMP'/reward.png"; plot "/dev/stdin";'
 done
