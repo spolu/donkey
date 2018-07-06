48faaa8605932e337168472509fd485597b3603b
Stan Polu <stan@stripe.com>
2018-07-06 14:02:30 +0200

    trying with a bigger VAE

--- reinforce/policies/vae_cropped_edges.py
+++ reinforce/policies/vae_cropped_edges.py
@@ -14,8 +14,8 @@ import reinforce
 
 # import pdb; pdb.set_trace()
 
-CONV_OUT_WIDTH = 15
-CONV_OUT_HEIGHT = 37
+CONV_OUT_WIDTH = 2
+CONV_OUT_HEIGHT = 8
 
 class VAECroppedEdges(nn.Module):
     def __init__(self, config):
@@ -25,17 +25,21 @@ class VAECroppedEdges(nn.Module):
         self.device = torch.device(config.get('device'))
 
         ## Encoder
-        self.cv1 = nn.Conv2d(1, 24, 5, stride=2)
-        self.cv2 = nn.Conv2d(24, 32, 5, stride=2)
+        self.cv1 = nn.Conv2d(1, 32, 4, stride=2)
+        self.cv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.cv3 = nn.Conv2d(64, 128, 4, stride=2)
+        self.cv4 = nn.Conv2d(128, 256, 4, stride=2)
 
         ## Latent representation of mean and std
-        self.fc1 = nn.Linear(32*CONV_OUT_WIDTH*CONV_OUT_HEIGHT, self.latent_size)
-        self.fc2 = nn.Linear(32*CONV_OUT_WIDTH*CONV_OUT_HEIGHT, self.latent_size)
-        self.fc3 = nn.Linear(self.latent_size, 32*CONV_OUT_WIDTH*CONV_OUT_HEIGHT)
+        self.fc1 = nn.Linear(256*CONV_OUT_WIDTH*CONV_OUT_HEIGHT, self.latent_size)
+        self.fc2 = nn.Linear(256*CONV_OUT_WIDTH*CONV_OUT_HEIGHT, self.latent_size)
+        self.fc3 = nn.Linear(self.latent_size, 256*CONV_OUT_WIDTH*CONV_OUT_HEIGHT)
 
         ## Decoder
-        self.dcv1 = nn.ConvTranspose2d(32, 24, (5, 6), stride=2)
-        self.dcv2 = nn.ConvTranspose2d(24, 1, 6, stride=2)
+        self.dcv1 = nn.ConvTranspose2d(256, 128, 4, stride=2, output_padding=(1,0))
+        self.dcv2 = nn.ConvTranspose2d(128, 64, 4, stride=2)
+        self.dcv3 = nn.ConvTranspose2d(64, 32, 4, stride=2, output_padding=(0,1))
+        self.dcv4 = nn.ConvTranspose2d(32, 1, 5, stride=2, output_padding=1, padding=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -51,20 +55,34 @@ class VAECroppedEdges(nn.Module):
 
     def encode(self, inputs):
         x = F.relu(self.cv1(inputs))
+        # print("cv1 {}".format(x.size()))
         x = F.relu(self.cv2(x))
+        # print("cv2 {}".format(x.size()))
+        x = F.relu(self.cv3(x))
+        # print("cv3 {}".format(x.size()))
+        x = F.relu(self.cv4(x))
+        # print("cv4 {}".format(x.size()))
 
         # import pdb; pdb.set_trace()
-        x = x.view(-1, 32*CONV_OUT_WIDTH*CONV_OUT_HEIGHT)
+        x = x.view(-1, 256*CONV_OUT_WIDTH*CONV_OUT_HEIGHT)
 
         return self.fc1(x), self.fc2(x)
 
     def decode(self, z):
         x = self.fc3(z)
 
-        x = x.view(-1, 32, CONV_OUT_WIDTH, CONV_OUT_HEIGHT)
+        x = x.view(-1, 256, CONV_OUT_WIDTH, CONV_OUT_HEIGHT)
 
-        x = F.tanh(self.dcv1(x))
-        x = F.sigmoid(self.dcv2(x))
+        x = F.relu(self.dcv1(x))
+        # print("dcv1 {}".format(x.size()))
+        x = F.relu(self.dcv2(x))
+        # print("dcv2 {}".format(x.size()))
+        x = F.relu(self.dcv3(x))
+        # print("dcv3 {}".format(x.size()))
+        x = F.sigmoid(self.dcv4(x))
+        # print("dcv4 {}".format(x.size()))
+
+        # import pdb; pdb.set_trace()
 
         return x
 
