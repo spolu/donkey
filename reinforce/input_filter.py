fb3c030ec0b8133de86b3971b2c7abdd57540e19
Stan Polu <stan@stripe.com>
2018-07-24 14:13:03 +0200

    fix warp_line_detector

--- reinforce/input_filter.py
+++ reinforce/input_filter.py
@@ -25,32 +25,34 @@ class InputFilter():
 
         if self.input_filter == 'warp_line_detector':
             pts1 = np.float32([
-                [13,33], [54,8], [147,33], [107,8]
+                [13, 33], [54, 8], [147, 33], [107, 8]
             ])
             pts2 = np.float32([
-                [60,80], [60,20], [120,80], [120,20]
+                [60, 80], [60, 20], [120, 80], [120, 20]
             ])
-            M = cv2.getPerspectiveTransform(pts1,pts2)
+            M = cv2.getPerspectiveTransform(pts1, pts2)
 
-            h_base = np.array([-1,-1,-1,-1,2,2,2,2,-1,-1,-1,-1])
+            h_base = np.array([
+                -1, -1, -1, -1, 2, 2, 2, 2, -1, -1, -1, -1,
+            ])
             arrays = [h_base for _ in range(12)]
             h_kernel = np.stack(arrays, axis=0)
 
             v_kernel = h_kernel.T
 
             d1_kernel = [
-                [ 2, 2, -1, -1, -1 , -1, -1, -1, -1 , -1, -1, -1],
-                [ 2, 2,  2, -1, -1 , -1, -1, -1, -1 , -1, -1, -1],
-                [-1, 2,  2,  2, -1 , -1, -1, -1, -1 , -1, -1, -1],
-                [-1,-1,  2,  2,  2 , -1, -1, -1, -1 , -1, -1, -1],
-                [-1,-1, -1,  2,  2 ,  2, -1, -1, -1 , -1, -1, -1],
-                [-1,-1, -1, -1,  2 ,  2,  2, -1, -1 , -1, -1, -1],
-                [-1,-1, -1, -1, -1 ,  2,  2,  2, -1 , -1, -1, -1],
-                [-1,-1, -1, -1, -1 , -1,  2,  2,  2 , -1, -1, -1],
-                [-1,-1, -1, -1, -1 , -1, -1,  2,  2 ,  2, -1, -1],
-                [-1,-1, -1, -1, -1 , -1, -1, -1,  2 ,  2,  2,  2],
-                [-1,-1, -1, -1, -1 , -1, -1, -1, -1 ,  2,  2,  2],
-                [-1,-1, -1, -1, -1 , -1, -1, -1, -1 , -1,  2,  2]
+                [ 2,  2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+                [ 2,  2,  2, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+                [-1,  2,  2,  2, -1, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1,  2,  2,  2, -1, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1,  2,  2,  2, -1, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1,  2,  2,  2, -1, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1,  2,  2,  2, -1, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1,  2,  2,  2, -1, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1,  2,  2,  2, -1, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1,  2,  2,  2, -1],
+                [-1, -1, -1, -1, -1, -1, -1, -1, -1,  2,  2,  2],
+                [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1,  2,  2]
             ]
             d2_kernel = np.fliplr(d1_kernel)
 
@@ -66,6 +68,9 @@ class InputFilter():
                 average_kernel,
             )
 
+            # Clamping is what gives us the very contrasted lines.
+            img = np.clip(img, 0, 255.0)
+
         if self.input_filter == 'line_detector':
             h_base = np.array([-1]*4 + [2]*8 + [-1]*4)
             arrays = [h_base for _ in range(16)]
@@ -74,11 +79,19 @@ class InputFilter():
             v_kernel = h_kernel.T
 
             # TODO(matthieu): Create an array for a diagonal filter kernel.
-            d1_array = [[-1]*max(i-3,0) + [2]*min(i+3,6,19-i) + [-1]*max(13 - i,0) for i in range(16)]
+
+            d1_array = [
+                [-1] * max(i-3, 0) +
+                [2] * min(i+3, 6, 19-i) +
+                [-1] * max(13-i, 0)
+                for i in range(16)
+            ]
             d1_kernel = np.stack(d1_array, axis=0)
             d2_kernel = np.fliplr(d1_kernel)
 
-            average_kernel = (v_kernel + h_kernel + d1_kernel + d2_kernel+0.5)/(2.0)
+            average_kernel = (
+                v_kernel + h_kernel + d1_kernel + d2_kernel + 0.5
+            ) / 2.0
 
             img = cv2.filter2D(img, -1, average_kernel)
 
