e1f00ae1248a82a83f827980926366600af76467
Stanislas Polu <polu.stanislas@gmail.com>
2018-07-27 16:25:10 +0200

    fix STL batch norm

--- synthetic/generator.py
+++ synthetic/generator.py
@@ -141,11 +141,11 @@ class STL(nn.Module):
         self.latent_size = config.get('latent_size')
 
         self.fc1 = nn.Linear(State.size(), self.latent_size, bias=False)
-        self.bn_fc1 = nn.BatchNorm2d(self.latent_size)
+        self.bn_fc1 = nn.BatchNorm1d(self.latent_size)
         self.fc2 = nn.Linear(self.latent_size, self.latent_size, bias=False)
-        self.bn_fc2 = nn.BatchNorm2d(self.latent_size)
+        self.bn_fc2 = nn.BatchNorm1d(self.latent_size)
         self.fc3 = nn.Linear(self.latent_size, self.latent_size, bias=False)
-        self.bn_fc3 = nn.BatchNorm2d(self.latent_size)
+        self.bn_fc3 = nn.BatchNorm1d(self.latent_size)
         self.fc4 = nn.Linear(self.latent_size, self.latent_size)
 
         for m in self.modules():
